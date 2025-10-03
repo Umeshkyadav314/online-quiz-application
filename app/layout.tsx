235167525b4a87e1e9224bc7cc042863baebe297
UmeshKumarY <Umesh.y@goroots.us>
2025-10-04 01:04:42 +0530

    fix error

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,9 +11,9 @@ import Header from "@/components/header"
 import { Suspense } from "react"
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
+  title: "Online Quiz Platform",
+  description: "Created with OQP",
+  generator: "OQP",
 }
 
 export default function RootLayout({
